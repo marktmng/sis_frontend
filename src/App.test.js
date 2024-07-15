f64e519f82345e5e467cea501eb13f2df077f2a5
Mark Tmng <mailsareaccepting@gmail.com>
2024-07-15 21:30:03 +1200

    installed many things

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,10 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(
-    <Router>
-      <App />
-    </Router>
-  );
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
