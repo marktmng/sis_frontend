f75ff6928070a6b9c7bbf7e70211c43d828d7f42
Mark Tmng <mailsareaccepting@gmail.com>
2024-07-15 21:39:39 +1200

    try again

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   render(
     <Router>
       <App />
     </Router>
   );
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Use waitFor to wait for the element to appear
+  await waitFor(() => {
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
-
-
