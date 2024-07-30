f29070516799e62ea0c2e8a7c90eee970bf4df7b
Roy Lee Flores <flores.roylee@gmail.com>
2024-07-30 23:27:32 +1200

    Done enhancing the Topbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Home(props) {
   // Define styles
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Topbar(props) {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">[SIS]</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">[SIS]</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/program">Program</Nav.Link>
-
             {/* Dropdown */}
             <NavDropdown title="Sections" id="basic-nav-dropdown">
               <NavDropdown.Item href="/lecturer">Lecturer</NavDropdown.Item>
-              <NavDropdown.Item href="/students">Students </NavDropdown.Item>
+              <NavDropdown.Item href="/students">Students</NavDropdown.Item>
               <NavDropdown.Item href="/parents">Parent</NavDropdown.Item>
               <NavDropdown.Divider /> {/* Divider */}
               <NavDropdown.Item href="/studentmark">
@@ -26,16 +24,15 @@ function Topbar(props) {
               </NavDropdown.Item>
               <NavDropdown.Item href="/tutionfee">Tution Fee</NavDropdown.Item>
             </NavDropdown>
-
             {/* Dropdown */}
             <NavDropdown title="Portal" id="basic-nav-dropdown">
               <NavDropdown.Item href="/login">Login</NavDropdown.Item>
               <NavDropdown.Item href="/registration">Register</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
